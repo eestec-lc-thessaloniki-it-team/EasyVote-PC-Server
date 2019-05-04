c646b08633d9f27ad5bcd2f85a3e9ed48ed6c65a
Fantomas4 <Fantomas4@users.noreply.github.com>
2019-05-04 16:04:03 +0300

    Added the Session class.

---
+++ easyvote/src/main/java/Logic/Session.java
@@ -0,0 +1,14 @@
+package Logic;
+
+// Class representing an individual voting session held during a General Meeting
+public class Session {
+
+  // type determines the kind of voting that will take place during
+  // the session. For example, ROP votes that require yes/no answers, Board Elections
+  // that require name answers etc.
+  private String type;
+
+  public Session() {
+
+  }
+}
