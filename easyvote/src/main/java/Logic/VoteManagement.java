2c862f9774c6934776da8fa7792d39c1f16dac26
Fantomas4 <Fantomas4@users.noreply.github.com>
2019-05-04 16:04:03 +0300

    Added the Session class.

--- easyvote/src/main/java/Logic/VoteManagement.java
+++ easyvote/src/main/java/Logic/VoteManagement.java
@@ -2,4 +2,11 @@ package Logic;
 
 public class VoteManagement {
 
+  public void createVotingSession(String typeOfSession) {
+    // typeOfSession determines the kind of voting that will take place during
+    // the session. For example, ROP votes that require yes/no answers, Board Elections
+    // that require name answers etc.
+
+  }
+
 }
