a2cd77adacbac998a580aeb1eacfb2bfa3df3d83
kwdikasGenitsi <kwdikasGenitsi@github.com>
2019-05-09 18:43:56 +0300

    Tst

--- easyvote/src/main/java/Logic/VoteManagement.java
+++ easyvote/src/main/java/Logic/VoteManagement.java
@@ -6,7 +6,7 @@ public class VoteManagement {
     // typeOfSession determines the kind of voting that will take place during
     // the session. For example, ROP votes that require yes/no answers, Board Elections
     // that require name answers etc.
-
+    // heekules
   }
 
 }
