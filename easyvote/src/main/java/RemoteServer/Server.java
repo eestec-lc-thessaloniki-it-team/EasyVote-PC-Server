506bffa33592436457271ae613dbcdfc69a6c9b3
kwdikasGenitsi <kwdikasGenitsi@github.com>
2019-05-20 22:37:16 +0300

    Add Server class

---
+++ easyvote/src/main/java/RemoteServer/Server.java
@@ -0,0 +1,11 @@
+package RemoteServer;
+
+public class Server {
+    void sendMessage() {
+        // TODO: implement me!
+    }
+
+    void receiveMessage() {
+        //TODO: implement me!
+    }
+}
