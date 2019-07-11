d2f4892074996e373777a263e44712e0af8553db
grode <dimitris23bp@gmail.com>
2019-07-11 18:25:20 +0300

    Some minor changes and more comments

--- easyvote/src/main/java/Logic/VoteManagement.java
+++ easyvote/src/main/java/Logic/VoteManagement.java
@@ -1,48 +1,48 @@
 package Logic;
 
 import java.util.ArrayList;
-
 import Logic.HandRules.HandRuleRequest;
 
 public class VoteManagement {
-	
-	private ArrayList<HandRuleRequest> requests = new ArrayList<>();
-	private HandRules handRules = new HandRules();
 
-	public void createVotingSession(String typeOfSession) {
-		// typeOfSession determines the kind of voting that will take place during
-		// the session. For example, ROP votes that require yes/no answers, Board
-		// Elections
-		// that require name answers etc.
+  // Array with every hand rule request that is active
+  private ArrayList<HandRuleRequest> requests = new ArrayList<>();
+  private HandRules handRules = new HandRules();
 
-	}
-	
-	//Add the new request and then sort them all again
-	public void handRuleAdded(String handRule, int id) {
-		requests.add(handRules.new HandRuleRequest(handRule, id));
-		requests.sort(handRules.comparator);
-		
-	}
-	
-	
-	public void changeHandRuleAttribute(String newAttr, int id) {
-		
-		switch (newAttr) {
-		case "FINISH_SPEAKING_TURN":
-			//Remove the first element
-			requests.remove(0);
-			break;
-		case "PASS_SPEAKING_TURN":
-			for(HandRuleRequest request : requests) {
-				if(request.getId() == id) {
-					request.setPass(false);
-				}
-			}
-		default:
-			System.out.println("Something went wrong.");
-			break;
-		}
-	}
+  public void createVotingSession(String typeOfSession) {
+    // typeOfSession determines the kind of voting that will take place during
+    // the session. For example, ROP votes that require yes/no answers, Board
+    // Elections
+    // that require name answers etc.
+
+  }
+
+  // Add the new request and then sort them all again
+  public void handRuleAdded(String handRule, int id) {
+    requests.add(handRules.new HandRuleRequest(handRule, id));
+    requests.sort(handRules.comparator);
+  }
+
+  //Change the attribute of the request based on user's option
+  public void changeHandRuleAttribute(String newAttr, int id) {
+
+    switch (newAttr) {
+      case "FINISH_SPEAKING_TURN":
+        // Remove the first element
+        requests.remove(0);
+        break;
+      case "PASS_SPEAKING_TURN":
+        for (int i = 0; i < requests.size(); i++) {
+          if (requests.get(i).getId() == id) {
+            //Remove the requested element as desired
+            requests.remove(i);
+          }
+        }
+      default:
+        System.out.println("Something went wrong.");
+        break;
+    }
+  }
 
 
 }
