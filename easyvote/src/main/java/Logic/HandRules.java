fb59f6a41da25f629a70ffc5cf13a9fbc7a277d0
grode <grode@pop-os.localdomain>
2019-07-11 18:25:20 +0300

    Some minor changes and more comments

--- easyvote/src/main/java/Logic/HandRules.java
+++ easyvote/src/main/java/Logic/HandRules.java
@@ -4,64 +4,60 @@ import java.util.Comparator;
 
 public class HandRules {
 
-	// Functional Interface of Comparator
-	Comparator<HandRuleRequest> comparator = new Comparator<HandRules.HandRuleRequest>() {
+  // Functional Interface of Comparator
+  Comparator<HandRuleRequest> comparator = new Comparator<HandRules.HandRuleRequest>() {
 
-		@Override
-		public int compare(HandRuleRequest req1, HandRuleRequest req2) {
-			if (req1.getPriority() > req2.getPriority()) {
-				return 1;
-			}
+    @Override
+    public int compare(HandRuleRequest req1, HandRuleRequest req2) {
+      //Greater priority equals to smaller index in the array
+      if (req1.getPriority() < req2.getPriority()) {
+        return -1;
+      }
 
-			return -1;
-		}
-	};
+      return 1;
+    }
+  };
 
-	public Comparator<HandRuleRequest> getComparator() {
-		return comparator;
-	}
+  public Comparator<HandRuleRequest> getComparator() {
+    return comparator;
+  }
 
-	// Every hand rule request is an instance of this class
-	public class HandRuleRequest {
+  // Every hand rule request is an instance of this class
+  public class HandRuleRequest {
 
-		private int priority;
-		private int id;
-		private boolean pass = false;
+    private int priority;
+    private int id;
 
-		public HandRuleRequest(String handRule, int id) {
+    public HandRuleRequest(String handRule, int id) {
 
-			switch (handRule) {
-			case "Technical":
-				priority = 4;
-				break;
-			case "Respone":
-				priority = 3;
-				break;
-			case "Question":
-				priority = 2;
-				break;
-			case "New Topic":
-				priority = 1;
+      switch (handRule) {
+        case "Technical":
+          priority = 4;
+          break;
+        case "Respone":
+          priority = 3;
+          break;
+        case "Question":
+          priority = 2;
+          break;
+        case "New Topic":
+          priority = 1;
 
-			}
+      }
 
-			this.id = id;
+      this.id = id;
 
-		}
-		
+    }
 
-		public int getPriority() {
-			return priority;
-		}
 
-		public int getId() {
-			return id;
-		}
-		
-		public void setPass(boolean pass) {
-			this.pass = pass;
-		}
+    public int getPriority() {
+      return priority;
+    }
 
-	}
+    public int getId() {
+      return id;
+    }
+
+  }
 
 }
