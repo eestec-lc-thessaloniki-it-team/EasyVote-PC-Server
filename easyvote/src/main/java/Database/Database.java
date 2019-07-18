fe785b3737437010ac58a9ae2682987a5c57dc33
grode <dimitris23bp@gmail.com>
2019-07-18 23:21:21 +0300

    Additions to database and remove of bash script (login with root from now on)

--- easyvote/src/main/java/Database/Database.java
+++ easyvote/src/main/java/Database/Database.java
@@ -1,11 +1,6 @@
 package Database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Properties;
+import java.sql.*;
 
 public class Database {
 
@@ -26,6 +21,20 @@ public class Database {
     return conn;
   }
 
+  // Does what it says
+  public void createDatabase() {
+    String command = "CREATE DATABASE IF NOT EXISTS eestec;";
+    try {
+      Statement statement = conn.createStatement();
+      statement.executeUpdate(command);
+      statement.executeUpdate("USE eestec;");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+  // Does what it says
   public void createTable() {
     String command = "CREATE TABLE IF NOT EXISTS eestecUsers(id INT AUTO_INCREMENT PRIMARY KEY,"
         + "name VARCHAR(20))";
@@ -39,6 +48,7 @@ public class Database {
 
   }
 
+  // Insert a new person in the database
   public void insertIntoDatabase(String name) {
     String command = "INSERT INTO eestecUsers(name) VALUES('" + name + "');";
 
@@ -50,22 +60,23 @@ public class Database {
     }
 
   }
-  
+
+  // Give an id and get the real name of the client
   public String getName(int id) {
-    
+
     String command = "SELECT name FROM eestecUsers WHERE id=" + id;
-    
+
     try {
       Statement statement = conn.createStatement();
       ResultSet rs = statement.executeQuery(command);
       rs.next();
-      
+
       return rs.getString("name");
-      
+
     } catch (SQLException e) {
       e.printStackTrace();
     }
-    
+
     return null;
 
   }
