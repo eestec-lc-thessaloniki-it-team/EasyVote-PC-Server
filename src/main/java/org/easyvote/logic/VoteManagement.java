1992237d499c2a1c638eed238641a5391c785f8d
Fantomas4 <stefanokf@auth.gr>
2019-08-06 04:01:01 +0300

    Made changes to wizard screen 1 UI.

--- src/main/java/org/easyvote/logic/VoteManagement.java
+++ src/main/java/org/easyvote/logic/VoteManagement.java
@@ -9,7 +9,7 @@ public class VoteManagement {
   private ArrayList<HandRuleRequest> requests = new ArrayList<>();
   private HandRules handRules = new HandRules();
 
-  public void createVotingSession(String typeOfSession) {
+  public void addVotingSession(String typeOfSession) {
     // typeOfSession determines the kind of voting that will take place during
     // the session. For example, ROP votes that require yes/no answers, Board
     // Elections
