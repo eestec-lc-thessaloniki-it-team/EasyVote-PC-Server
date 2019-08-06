6554f9f103ce2f82527944b415564e1038107a21
Fantomas4 <stefanokf@auth.gr>
2019-08-06 04:15:01 +0300

    Worked on the implementation of the app's logic classes. Added VoteType enum class.

--- src/main/java/org/easyvote/logic/Session.java
+++ src/main/java/org/easyvote/logic/Session.java
@@ -1,14 +1,26 @@
 package org.easyvote.logic;
 
+import java.util.ArrayList;
+
 // Class representing an individual voting session held during a General Meeting
 public class Session {
 
   // type determines the kind of voting that will take place during
   // the session. For example, ROP votes that require yes/no answers, Board Elections
   // that require name answers etc.
-  private String type;
 
-  public Session() {
+  private String title;
+  private String type;
+  private ArrayList<Topic> topics;
+
+
+  public Session(String title, String type){
+    this.title = title;
+    this.type = type;
+    topics = new ArrayList<>();
+  }
+
+  public void addTopic(String title, String description, VoteType voteType){
 
   }
 }
---
+++ src/main/java/org/easyvote/logic/VoteType.java
@@ -0,0 +1,7 @@
+package org.easyvote.logic;
+
+enum VoteType {
+    YES_NO_ABSTAIN,
+    MULTIPLE_CHOICE
+
+}
